81ae865e5f1666418fb9ffa250eacec15a5526a8
Quentin Caritey <quentin.caritey@neoxia.com>
2022-08-30 09:55:25 +0200

    medical record screen

--- screens/MedicalRecordScreen.tsx
+++ screens/MedicalRecordScreen.tsx
@@ -1,18 +1,69 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, Pressable } from 'react-native';
+import { Card, Paragraph, List } from 'react-native-paper';
+import * as FileSystem from 'expo-file-system';
 
 import { View } from '../components/Themed';
 
 export default function MedicalRecordScreen() {
   return (
     <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title="Pépito" subtitle="Chien"
+          left={() =>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/shiba.jpg')}
+            />}
+        />
+        <Card.Content>
+          <Paragraph>Mâle</Paragraph>
+          <Paragraph>01/01/1990</Paragraph>
+        </Card.Content>
+      </Card>
+      <List.Section>
+        <List.Subheader>Derniers rendez-vous</List.Subheader>
+        <List.Item
+          title="Vaccin contre xxx - Vaccination"
+          description="Quentin Caritey - 14 août 2022 18:58"
+          left={() => <List.Icon icon="calendar" />}
+        />
+        <List.Item
+          title="Mensuel - Consultation"
+          description="Quentin Caritey - 15 août 2022 14:22"
+          left={() => <List.Icon icon="calendar" />}
+        />
+      </List.Section>
+      <List.Section>
+        <List.Subheader>Documents</List.Subheader>
+        <List.Item
+          title="Ordonnance"
+          description="14 août 2022 22:51"
+          left={() => <List.Icon icon="file-document" />}
+          right={() => <Pressable onPress={async () => {
+            return FileSystem
+              .downloadAsync('http://www.africau.edu/images/default/sample.pdf', FileSystem.documentDirectory + 'sample.pdf');
+          }}>
+            <List.Icon icon="arrow-collapse-down" />
+          </Pressable>}
+        />
+      </List.Section>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    display: 'flex',
+    width: '100%',
+    height: '100%'
+  },
+  card: {
+    margin: 10
+  },
+  image: {
+    marginRight: 20,
+    height: 45,
+    width: 45,
+    borderRadius: 100
+  },
 });
